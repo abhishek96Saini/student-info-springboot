0194a64f6fd340cbbea929b4418d5db6cfcc6850
Abhishek Saini <abhishek.saini@geminisolutions.in>
2020-04-03 19:46:10 +0530

    swagger-ui documentation

--- src/main/java/com/abhishek/studentproject/studentinfo/studrestcontroller/StudentRestController.java
+++ src/main/java/com/abhishek/studentproject/studentinfo/studrestcontroller/StudentRestController.java
@@ -4,10 +4,12 @@ import com.abhishek.studentproject.studentinfo.Student;
 import com.abhishek.studentproject.studentinfo.service.AppService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.HashMap;
 
 @RestController
+@EnableSwagger2
 @RequestMapping("/student")  // mapping this class at "/student"
 public class StudentRestController {
 
